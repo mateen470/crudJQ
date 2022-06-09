3080d619667e67771aa26e0d55f465e076b8f792
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-06-09 18:14:12 +0500

    completion

--- script.js
+++ script.js
@@ -1,75 +1,71 @@
 $(document).ready(function () {
-    let i = 1;
+  let i = 1;
 
-    $("#btnAdd").click(function () {
-      let newHtml = GetHtml()
-        .replace("$[Id]", i++)
-        .replace("$[Name]", $("#txtname").val())
-        .replace("$[role]", $("#ddlRole").val())
-        .replace("$[rolex]", $("#nme").val())
-        .replace("$[action]", "Edit")
-        .replace("$[Delete]", "Delete");
+  $("#btnAdd").click(function () {
+    let newHtml = GetHtml()
+      .replace("$[Id]", i++)
+      .replace("$[Product]", $("#_name").val())
+      .replace("$[Qunatity]", $("#_quantity").val())
+      .replace("$[Price]", $("#_price").val())
+      .replace("$[action]", "Edit")
+      .replace("$[Delete]", "Delete");
 
-      $("#table_td").append(newHtml);
-    });
-
-    $("#btnUpdate").click(function () {
-      let newHtml = GetHtml()
-        .replace("$[Id]", $("#txtId").val())
-        .replace("$[Name]", $("#txtname").val())
-        .replace("$[role]", $("#ddlRole").val())
-        .replace("$[rolex]", $("#nme").val())
-        .replace("$[action]", "Edit")
-        .replace("$[Delete]", "Delete");
-
-      $("#table_td").append(newHtml);
-
-      $("#btnAdd").show();
-
-      $("#btnUpdate").hide();
-
-      $("#txtId").val("");
-
-      $("#txtname").val("");
-
-      $("#ddlRole").val("");
-      $("#nme").val("");
-    });
-
-    $(document).on("click", "#A_Edit", function () {
-      $("#btnAdd").hide();
-
-      $("#btnUpdate").show();
-
-      $("#txtId").show();
-
-      $("#txtId").val($(this).parent().parent().find(".Id").html());
-
-      $("#txtname").val($(this).parent().parent().find(".Name").html());
-
-      $("#ddlRole").val(
-        $(this).parent().parent().find(".role").html()
-      );
-      $("#nme").val(
-        $(this).parent().parent().find(".rolex").html()
-      );
-
-      $(this).parents("tr").remove();
-
-      $("#btnUpdate").focus();
-    });
-
-    $(document).on("click", "#A_delete", function () {
-      $(this).parents("tr").remove();
-    });
+    $("#table_td").append(newHtml);
   });
 
-  function GetHtml() {
-    return (
-      "<tr><td class='Id'>$[Id]</td>" +
-      "<td class='Name'>$[Name]</td>" +
-      "<td class='role'>$[role]</td>" +
-      "<td class='rolex'>$[rolex]</td>" +
-      "<td class='action'><a href='#' id='A_Edit'> $[action] </a> <a href='#' id='A_delete'>$[Delete]</a></td></tr>"
-    );
-  }
+  $("#btnUpdate").click(function () {
+    let newHtml = GetHtml()
+      .replace("$[Id]", $("#_id").val())
+      .replace("$[Product]", $("#_name").val())
+      .replace("$[Qunatity]", $("#_quantity").val())
+      .replace("$[Price]", $("#_price").val())
+      .replace("$[action]", "Edit")
+      .replace("$[Delete]", "Delete");
+
+    $("#table_td").append(newHtml);
+
+    $("#btnAdd").show();
+
+    $("#btnUpdate").hide();
+
+    $("#_id").val("");
+
+    $("#_name").val("");
+
+    $("#_quantity").val("");
+    $("#_price").val("");
+  });
+
+  $(document).on("click", "#A_Edit", function () {
+    $("#btnAdd").hide();
+
+    $("#btnUpdate").show();
+
+    $("#_id").show();
+
+    $("#_id").val($(this).parent().parent().find(".Id").html());
+
+    $("#_name").val($(this).parent().parent().find(".Product").html());
+
+    $("#_quantity").val($(this).parent().parent().find(".Qunatity").html());
+    $("#_price").val($(this).parent().parent().find(".Price").html());
+
+    $(this).parents("tr").remove();
+
+    $("#btnUpdate").focus();
+  });
+
+  $(document).on("click", "#A_delete", function () {
+    $(this).parents("tr").remove();
+  });
+});
+
+function GetHtml() {
+  return (
+    "<tr><td class='Id'>$[Id]</td>" +
+    "<td class='Name'>$[Product]</td>" +
+    "<td class='role'>$[Qunatity]</td>" +
+    "<td class='rolex'>$[Price]</td>" +
+    "<td class='action'><a href='#' id='A_Edit'> $[action] </a> <a href='#' id='A_delete'>$[Delete]</a></td></tr>"
+  );
+}
